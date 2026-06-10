f003c4a8b072bef04c16717f7a7a8a76c041f62e
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-06-11 00:14:46 +1000

    style: format tsdown.config.ts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
     attw: true,
-    dts: { sourcemap: true },
-    exports: { devExports: true },
+    dts: {
+        sourcemap: true,
+    },
+    exports: {
+        devExports: true,
+    },
     format: ['cjs', 'esm'],
     publint: true,
     tsconfig: 'tsconfig.build.json',
